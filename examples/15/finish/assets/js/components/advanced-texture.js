a09b3363f725cdc8960d8968a26e2f3183780578
Michael Andrew <michael@gladeye.co.nz>
2017-04-20 16:12:49 +1200

    Added shadow support and inspector bug workaround
    Now shows shadows on example 15 and the Bootstrap modal works correctly with the a-frame inspector. Still having some CSS collision issues with the Bootstrap modal styles getting overriden by the inspector CSS.

---
+++ examples/15/finish/assets/js/components/advanced-texture.js
@@ -0,0 +1,72 @@
+/**
+ * Advanced Texture component
+ *
+ * Use this in place of loading in a model's UV texture. It will replace the uv texture on all the materials in the
+ * model with the one specified via the texture property of this component.
+ *
+ * e.g: advanced-texture="src: RELATIVE_PATH_TO_TEXTURE"
+ *
+ * Note that src cannot be a cached ID reference to an asset in the scene
+ *
+ *
+ */
+AFRAME.registerComponent('advanced-texture', {
+    schema: {
+        texture:         { type: 'asset', required: true },
+        crossorigin: { default: '' },
+        shininess: { default: 30 }
+    },
+    /**
+     * `init` used to initialize material. Called once.
+     */
+    init: function () {
+
+        var self = this;
+
+        this.el.addEventListener('model-loaded',function(e){
+
+            var url = self.convertToAbsoluteURL(document.baseURI,self.data.src);
+
+            var loader = new THREE.TextureLoader();
+            loader.load(url, function ( texture ) {
+                //var material = new THREE.MeshLambertMaterial({map: texture, vertexColors: THREE.VertexColors});
+                //material.shading = THREE.SmoothShading;
+                e.target.object3D.traverse(function(child){
+
+                    if(child instanceof THREE.Mesh){
+                        child.material.shading = THREE.SmoothShading;
+                        child.material.map = texture;
+                        child.material.needsUpdate = true;
+                        child.material.shininess = self.data.shininess;
+
+
+                        //child.material = material;
+
+                    }
+                });
+            });
+
+
+
+        });
+
+
+    },
+
+    convertToAbsoluteURL: function(base, relative) {
+        var stack = base.split("/"),
+            parts = relative.split("/");
+        stack.pop(); // remove current file name (or empty string)
+                     // (omit if "base" is the current folder without trailing slash)
+        for (var i=0; i<parts.length; i++) {
+            if (parts[i] == ".")
+                continue;
+            if (parts[i] == "..")
+                stack.pop();
+            else
+                stack.push(parts[i]);
+        }
+        return stack.join("/");
+    }
+});
+
