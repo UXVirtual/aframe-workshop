c579b785cbb2651d7126a767eaa53b68a2106941
Michael Andrew <michael@gladeye.co.nz>
2017-04-21 20:06:02 +1200

    Example 14 now working

---
+++ examples/14/finish/assets/js/main.js
@@ -0,0 +1,83 @@
+
+// Define custom schema for syncing avatar color, set by random-color
+var avatarSchema = {
+    template: '#avatar-template',
+    components: [
+        'position',
+        'rotation',
+        {
+            selector: '.head',
+            component: 'material'
+        }
+    ]
+};
+NAF.schemas.add(avatarSchema);
+
+function randomPointOnCircle(radius, angleRad) {
+    x = Math.cos(angleRad)*radius;
+    y = Math.sin(angleRad)*radius;
+    return {x: x, y: y};
+}
+
+// Called by Networked-Aframe when connected to server
+function onConnect(e) {
+
+    console.log('Client connected.',e );
+
+    // Get random angle
+    var angleRad = Math.random()*Math.PI*2;
+
+    // Get position around a circle
+    var position = randomPointOnCircle(3, angleRad);
+    var positionStr = position.x + ' 1.3 ' + position.y;
+
+    // Get rotation towards center of circle
+    var angleDeg = angleRad * 180 / Math.PI;
+    var angleToCenter = -1 * angleDeg + 90;
+    var rotationStr = '0 ' + angleToCenter + ' 0';
+
+    // Create avatar with this position and rotation
+    console.log('Creating avatar...');
+    NAF.entities.createAvatar('#avatar-template', positionStr, rotationStr);
+
+    NAF.connection.isConnected();
+}
+
+AFRAME.registerSystem('main', {
+    schema: {},  // System schema. Parses into `this.data`.
+    init: function () {
+        // Called on scene initialization.
+
+        //do stuff here after scene initializes
+
+        var self = this;
+
+        function checkConnected() {
+            return new Promise(function (resolve, reject) {
+                (function waitForConnection(){
+                    console.log('Waiting for connection...');
+                    if (NAF.connection.isConnected()) return resolve();
+                    setTimeout(waitForConnection, 500);
+                })();
+            });
+        }
+
+        checkConnected(function(){
+            console.log('Connected to server. Can begin') ;
+            self.begin();
+        },function(e){
+            console.log('An error ocurred',e);
+        });
+    },
+
+    begin: function() {
+        console.log('Beginning...');
+    },
+
+    tick: function (t, dt) {
+
+
+    }
+    // Other handlers and methods.
+});
+
