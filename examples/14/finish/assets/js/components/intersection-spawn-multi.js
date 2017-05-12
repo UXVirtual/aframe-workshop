41f57c391e5648da8dc9d85c96af04505c45341b
Michael Andrew <michael@gladeye.co.nz>
2017-05-12 21:13:46 +1200

    Added support for spawning random block variant
    Also improved lighting on example 14

--- examples/14/finish/assets/js/components/intersection-spawn-multi.js
+++ examples/14/finish/assets/js/components/intersection-spawn-multi.js
@@ -1,51 +1,50 @@
 /**
  * Spawn entity at the intersection point on click, given the properties passed.
  *
- * `<a-entity intersection-spawn="mixin: box; material.color: red">` will spawn
+ * `<a-entity intersection-spawn-multi="mixin: box; material.color: red">` will spawn
  * `<a-entity mixin="box" material="color: red">` at intersection point.
  */
 AFRAME.registerComponent('intersection-spawn-multi', {
   dependencies: ['position'],
   schema: {
-    default: '',
-    parse: AFRAME.utils.styleParser.parse,
-    template: { type: 'string', required: true },
-    offset: {type: 'vector3'},
-    snap: {type: 'vector3'}
+    templates: {
+      type: 'array',
+      required: true
+    },
+    offset: {type: 'vec3', default: {x: 0.25, y: 0.25, z: 0.25}},
+    snap: {type: 'vec3', default: {x: 0.5, y: 0.5, z: 0.5}}
   },
 
   init: function () {
     const data = this.data;
     const el = this.el;
-    const self = this;
-      this.originalPos = this.el.getAttribute('position');
 
-    var templateSchema = {
-      template: data.template,
-      components: [
-        'position',
-      ]
-    };
-    NAF.schemas.add(templateSchema);
+    for(var i = 0; i < data.templates.length; i++){
+
+      var templateSchema = {
+        template: data.templates[i],
+        components: [
+          'position'
+        ]
+      };
+      NAF.schemas.add(templateSchema);
+    }
 
     NAF.options.compressSyncPackets = true;
     NAF.options.updateRate = 1;
 
     el.addEventListener(data.event, function(evt){
-      // Create element.
 
       var worldPos = evt.detail.intersection.point;
-
       const pos = AFRAME.utils.clone(worldPos);
 
-      pos.x   = Math.floor(pos.x / 0.5) * 0.5 + 0.25;
-      pos.y = Math.floor(pos.y / 0.5) * 0.5 + 0.25;
-      pos.z = Math.floor(pos.z / 0.5) * 0.5 + 0.25;
+      pos.x   = Math.floor(pos.x / data.snap.x) * data.snap.x + data.offset.x;
+      pos.y = Math.floor(pos.y / data.snap.y) * data.snap.y+ data.offset.y;
+      pos.z = Math.floor(pos.z / data.snap.z) * data.snap.z + data.offset.z;
 
-      var spawnEl = NAF.entities.createNetworkEntity("#voxel-template", pos, '0 0 0');
-      NAF.utils.whenEntityLoaded(spawnEl, function() {
-
-      });
+      var randomInt = Math.floor(Math.random() * (data.templates.length - 1 + 1)) + 0;
+      var spawnEl = NAF.entities.createNetworkEntity(data.templates[randomInt], pos, '0 0 0');
+      NAF.utils.whenEntityLoaded(spawnEl, function() {});
     });
   }
 });
