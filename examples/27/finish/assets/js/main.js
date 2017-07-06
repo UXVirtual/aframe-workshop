4864efae8646d30500bf441fa7dfadc844e60a58
Michael Andrew <michael@gladeye.co.nz>
2017-07-06 18:50:02 +1200

    Added grading and audio to example 27

--- examples/27/finish/assets/js/main.js
+++ examples/27/finish/assets/js/main.js
@@ -5,8 +5,6 @@ AFRAME.registerSystem('main', {
 
         //do stuff here after scene initializes
 
-        this.$modal = $('#hotspot-modal');
-        this.$originalModal = this.$modal.clone();
         this.lastInspectorState = 'closed';
 
         var self = this;
@@ -28,75 +26,16 @@ AFRAME.registerSystem('main', {
             if(targetElClass === 'hotspot'){
 
                 if(!rotating){
-                    rotating = true;
-                    camera.setAttribute('orbit-controls', {
-                        rotateTo: targetEl.getAttribute('rotateToPosition')
-                    });
 
-                    setTimeout(function(){
-                        sceneEl.pause();
-                        self.$modal.modal('show');
-                        rotating = false;
-                    },2000);
                 }
-
-
-
-                //console.log(targetEl.getAttribute('rotateToPosition'))
-                //console.log(camera.getAttribute('orbit-controls'))
             }
         });
 
-        this.$modal.modal({
-            show: false,
-            backdrop: true
-        });
-
-        this.$modal.on('hidden.bs.modal', function (e) {
-            sceneEl.play();
-
-            var orbitControlsComponent = camera.components['orbit-controls'];
-
-            //reset the orbit controls component state after resuming the scene so it continues to auto-rotate
-            orbitControlsComponent.state = orbitControlsComponent.STATE.NONE;
-        });
-
-        //wait for inspector to open / close
-        setInterval(this.onInspector,1000);
     },
 
     tick: function (t, dt) {
 
 
-    },
-    // Other handlers and methods.
-
-    onInspector: function() {
-        if(AFRAME.INSPECTOR && AFRAME.INSPECTOR.opened){
-            if(this.lastInspectorState !== 'opened'){
-                this.$modal.remove();
-                this.lastInspectorState = 'opened';
-            }
-
-        }else{
-            if(this.lastInspectorState !== 'closed'){
-
-                var $body = $('body');
-
-                if($body.length > 0) {
-
-                    //$('head style').remove();
-                    var hs = document.querySelectorAll('style');
-                    for (var i = 0, max = hs.length; i < max - 1; i++) {
-                        hs[i].parentNode.removeChild(hs[i]);
-                    }
-                    $body.append(this.$originalModal);
-                    this.$modal = $('#hotspot-modal');
-                    this.lastInspectorState = 'closed';
-                }
-            }
-
-        }
     }
 });
 
