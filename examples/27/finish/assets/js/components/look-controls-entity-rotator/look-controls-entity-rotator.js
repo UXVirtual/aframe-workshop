a39943e59aa8685501eb29678c59efdc11e03d66
Michael Andrew <michael@gladeye.co.nz>
2017-07-11 14:41:23 +1200

    Smoothing object rotation every 5 frames

--- examples/27/finish/assets/js/components/look-controls-entity-rotator/look-controls-entity-rotator.js
+++ examples/27/finish/assets/js/components/look-controls-entity-rotator/look-controls-entity-rotator.js
@@ -12,7 +12,7 @@ if (typeof AFRAME === 'undefined') {
 AFRAME.registerComponent('look-controls-entity-rotator', {
     schema: {
         target: {type: 'selector', required: true},
-        smoothing: {type: 'int', default: 100}
+        smoothing: {type: 'int', default: 5}
     },
 
     /**
@@ -46,19 +46,81 @@ AFRAME.registerComponent('look-controls-entity-rotator', {
      * Called on each scene tick.
      */
     tick: function (t) {
-        this.setTargetRotation(t);
+        //this.setTargetRotation(t);
 
-        //this.setTargetRotationSmoothed();
+        this.setTargetRotationSmoothed();
+    },
+
+    rotate: function(el, rotation, options) {
+        options || (options = {duration: 300});
+
+        var object = el.object3D;
+
+        //var origpos = new THREE.Vector3().copy(camera.position); // original position
+        var origrot = new THREE.Euler().copy(object.rotation); // original rotation
+
+        //camera.position.set(dstpos.x, dstpos.y, dstpos.z);
+        //object.lookAt(dstlookat);
+        var dstrot = new THREE.Euler().copy(rotation)
+
+        // reset original position and rotation
+        //camera.position.set(origpos.x, origpos.y, origpos.z);
+        object.rotation.set(origrot.x, origrot.y, origrot.z);
+
+        //
+        // Tweening
+        //
+
+        // position
+        /*new TWEEN.Tween(camera.position).to({
+            x: dstpos.x,
+            y: dstpos.y,
+            z: dstpos.z
+        }, options.duration).start();;*/
+
+        // rotation (using slerp)
+        (function () {
+            var qa = object.quaternion; // src quaternion
+            var qb = new THREE.Quaternion().setFromEuler(dstrot); // dst quaternion
+            var qm = new THREE.Quaternion();
+
+            var o = {t: 0};
+            new TWEEN.Tween(o).to({t: 1}, options.duration).onUpdate(function () {
+                THREE.Quaternion.slerp(qa, qb, qm, o.t);
+                object.quaternion.set(qm.x, qm.y, qm.z, qm.w);
+            }).onComplete(function(){
+                //el.setAttribute('rotation',rotation.x+' '+rotation.y+' '+rotation.z);
+            }).start();
+        }).call(this);
     },
 
     setTargetRotation: function(t){
 
-        if(this.tickCount < 10){
+        const currentModelRotation = this.targetEl.getAttribute('rotation');
+        const currentCameraRotation = this.el.getAttribute('rotation');
+
+        const newRotation = {
+            x: Math.abs(currentCameraRotation.x*-10),
+            y: Math.abs(currentCameraRotation.y*-10),
+            z: 0
+        };
+
+        //console.log(currentModelRotation,currentCameraRotation);
+
+        if(this.tickCount < 20){
             this.tickCount++;
         }else{
             this.tickCount = 0;
-            const rotation = this.el.getAttribute('rotation');
-            this.targetEl.setAttribute('rotation',rotation.x*-10+' '+rotation.y*-10+' 0');
+
+            console.log('Updating rotation');
+
+            //this.rotate(this.targetEl,newRotation,{duration:250});
+
+            setTimeout(function(){
+                this.targetEl.setAttribute('rotation',currentCameraRotation.x*-10+' '+currentCameraRotation.y*-10+' 0');
+            }.bind(this),1000);
+
+
         }
 
 
