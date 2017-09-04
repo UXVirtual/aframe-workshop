324b0b101c5039480300791f9d228ec941a459d5
Michael Andrew <michael.andrew@gladeye.com>
2017-09-04 21:19:25 +1200

    Now shows preloader animation for emulator

--- examples/29/finish/assets/js/components/canvas-surface.js
+++ examples/29/finish/assets/js/components/canvas-surface.js
@@ -6,31 +6,40 @@ AFRAME.registerComponent('canvas-surface', {
     schema: { },
     init: function() {
         this.el.addEventListener('draw-render', this.render.bind(this))
-        this.origCanvasEl = document.querySelector('#fullscreen');
-        this.ctx2 = this.origCanvasEl.getContext("2d");
         this.idata = null;
         this.ctx = null;
+
+        console.log('Canvas el: ',this.origCanvasEl);
+
+        this.origCanvasEl = document.querySelector('#fullscreen');
+        this.ctx2 = this.origCanvasEl.getContext("2d");
+
     },
     update: function() { },
     remove: function() { },
     pause: function() { },
     play: function() { },
     render: function(e) {
-        this.ctx = e.detail.ctx;
+
+
+        //
+
+
+        //console.log('2d context: ',this.ctx2);
+
+        if(this.ctx2){
+            this.ctx = e.detail.ctx;
             var texture = e.detail.texture;
             var w = this.ctx.canvas.width;
             var h = this.ctx.canvas.height;
-            //buffer32 = new Uint32Array(idata.data.buffer),
-            //len = buffer32.length,
-            //i = 0;
-        //for(; i < len;)
-            //buffer32[i++] = ((255 * Math.random())|0) << 24
+
+            this.idata = this.ctx2.getImageData(0, 0, w, h);
+
+            this.ctx.putImageData(this.idata, 0, 0);
+            // texture upate
+            texture.needsUpdate = true;
+        }
 
 
-        this.idata = this.ctx2.getImageData(0, 0, w, h);
-
-        this.ctx.putImageData(this.idata, 0, 0);
-        // texture upate
-        texture.needsUpdate = true
     }
 });
--- examples/29/finish/assets/js/main.js
+++ examples/29/finish/assets/js/main.js
@@ -1,3 +1,64 @@
-/**
- * Created by michael on 23/02/17.
- */
+AFRAME.registerSystem('main', {
+    schema: {},  // System schema. Parses into `this.data`.
+
+    init: function () {
+
+        var sceneEl = document.querySelector('a-scene');
+
+        console.log('el: ',sceneEl);
+
+        sceneEl.addEventListener('renderstart',function(){
+
+            console.log('Ready');
+
+            //mess binaries can be downloaded from: https://archive.org/details/emularity_engine_jsmame
+            var emulator = new Emulator(document.querySelector("#fullscreen"),
+                {
+                    before_emulator: function(){
+                        console.log('On before emulator callback');
+                    },
+                    before_run: this.onBeforeRun
+                },
+                new JSMAMELoader(JSMAMELoader.driver("bublbobl"),
+                    JSMAMELoader.nativeResolution(256, 256),
+                    JSMAMELoader.emulatorJS("assets/js/emularity/emulators/jsmess/mamebublbobl.js.gz"), //https://archive.org/download/emularity_engine_v1/mame1943.js.gz
+                    JSMAMELoader.mountFile("bublbobl.zip",
+                        JSMAMELoader.fetchFile("Game File",
+                            "assets/js/emularity/emulators/jsmess/bublbobl.zip"))))
+            emulator.setScale(1);
+            emulator.start({ waitAfterDownloading: false });
+
+            var scWidgetEl = document.querySelector('#sc-widget');
+
+
+            var rnd = Math.floor(Math.random() * 9) + 0;
+
+            scWidgetEl.setAttribute('src','https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/349520772&amp;color=ff5500&amp;auto_play=true&amp;start_track='+rnd+'&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=false')
+
+            var scWidget = SC.Widget(scWidgetEl);
+            //scWidget.setVolume(0.2);
+
+            scWidget.bind(SC.Widget.Events.READY, function() {
+                console.log('Soundcloud player widget ready');
+                scWidget.setVolume(30);
+            });
+        }.bind(this));
+
+
+
+    },
+
+    onBeforeRun: function(){
+        console.log('On before run callback');
+        var screenEl = document.querySelector('#area-light2');
+        console.log('screenEL',screenEl);
+        screenEl.setAttribute('material','shader','draw'); //switch from the gif material shader to the emulator canvas shader
+    },
+
+    tick: function (t, dt) {
+
+
+    }
+});
+
+
