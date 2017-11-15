0f29119ebea9f2e4826fd014fa69e3feda129bdb
Michael <michael@lightspeedgraphics.co.nz>
2017-11-15 15:08:52 +1300

    Added example 32 PBR rendering
    Duplicates Babylon.js demo found here: https://www.babylonjs.com/demos/pbrglossy/

---
+++ examples/32/finish/assets/js/main.js
@@ -0,0 +1,20 @@
+AFRAME.registerSystem('main', {
+  schema: {},  // System schema. Parses into `this.data`.
+  init: function () {
+    // Called on scene initialization.
+
+    //do stuff here after scene initializes
+
+
+
+
+
+  },
+  tick: function () {
+
+    //console.log('Tick');
+
+    //do stuff here every tick
+  }
+  // Other handlers and methods.
+});
