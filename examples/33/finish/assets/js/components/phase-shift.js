86ed9e63067bf5b30303ef0c003c0b5000ff808c
Michael <hazardu5@gmail.com>
2017-11-22 22:51:00 +1300

    Added Example 33 physics object shelf

---
+++ examples/33/finish/assets/js/components/phase-shift.js
@@ -0,0 +1,12 @@
+// turn controller's physics presence on only while button held down
+AFRAME.registerComponent('phase-shift', {
+    init: function () {
+        var el = this.el;
+        el.addEventListener('gripdown', function () {
+            el.setAttribute('collision-filter', {collisionForces: true});
+        });
+        el.addEventListener('gripup', function () {
+            el.setAttribute('collision-filter', {collisionForces: false});
+        });
+    }
+});
